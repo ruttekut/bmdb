70a4cad75d09bfe9d4d84c36ebdc3de1d5c68d18
Rutger Muller <rutger@Rutgers-MacBook-Pro.local>
2017-06-07 23:28:01 +0200

    Created columns on the landing page with categories of book from the database. Also created a userProfile linked to User now when a user is created you can link it to a profile see docs

--- account/models.py
+++ account/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -16,3 +18,19 @@ class Books(models.Model):
 
     def __str__(self):
             return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    description = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
